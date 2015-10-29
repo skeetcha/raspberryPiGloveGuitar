a76e577a4ec041d209e655c2dea846b8a06e205c
theMusicalGamer <pianojammer@comcast.net>
2015-10-29 10:13:27 -0700

    Adding String Gauge and breakage code

--- guitar/guitar.ino
+++ guitar/guitar.ino
@@ -1,7 +1,10 @@
 #include <SPI.h>
+#include <EEPROM.h>
 
 #define leftAccel 7
 #define rightAccel 6
+
+#define GAUGE 0
 
 int flex1 = A0;
 int flex2 = A1;
@@ -22,6 +25,8 @@ float scaledX1, scaledY1, scaledZ1, scaledX2, scaledY2, scaledZ2;
 char values[10];
 float scale = 0.0078;
 int flex1Read, flex2Read, flex3Read, flex4Read;
+
+int address = 0;
 
 void setup()
 {
@@ -49,6 +54,7 @@ void setup()
 
 void loop()
 {
+  eepromLoop();
   flexTest();
   
   Serial.print("Flex 1: ");
@@ -93,6 +99,92 @@ void loop()
   Serial.println("g");
   Serial.println();
   delay(500);
+}
+
+void eepromLoop()
+{
+  unsigned long time = millis();
+  unsigned long prevTime = EEPROM.read(address);
+  
+  EEPROM.write(address, prevTime + time);
+  
+  Serial.print("On Gauge ");
+  Serial.println(GAUGE);
+  Serial.print("Time until strings have broken: ");
+  
+  if (GAUGE == 0)
+  {
+    Serial.print((72000000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  else if (GAUGE == 1)
+  {
+    Serial.print((79200000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  else if (GAUGE == 2)
+  {
+    Serial.print((86400000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  else if (GAUGE == 3)
+  {
+    Serial.print((93600000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  else if (GAUGE == 4)
+  {
+    Serial.print((100800000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  else if (GAUGE == 5)
+  {
+    Serial.print((108000000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  else if (GAUGE == 6)
+  {
+    Serial.print((115200000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  else if (GAUGE == 7)
+  {
+    Serial.print((122400000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  else if (GAUGE == 8)
+  {
+    Serial.print((129600000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  else if (GAUGE == 9)
+  {
+    Serial.print((136800000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  else if (GAUGE == 10)
+  {
+    Serial.print((144000000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  else if (GAUGE == 11)
+  {
+    Serial.print((151200000 - (prevTime + time)) / 3600000);
+    Serial.println(" hours");
+  }
+  
+  unsigned long int timeNow = EEPROM.read(address);
+  
+  if ((GAUGE == 0 && timeNow == 72000000) || (GAUGE == 1 && timeNow == 79200000) || (GAUGE == 2 && timeNow == 86400000) || (GAUGE == 3 && timeNow == 93600000) || (GAUGE == 4 && timeNow == 100800000) || (GAUGE == 5 && timeNow == 108000000) || (GAUGE == 6 && timeNow == 115200000) || (GAUGE == 7 && timeNow == 122400000) || (GAUGE == 8 && timeNow == 129600000) || (GAUGE == 9 && timeNow == 136800000) || (GAUGE == 10 && timeNow == 144000000) || (GAUGE == 11 && timeNow == 151200000)
+  {
+    Serial.println("Strings broke");
+  }
+  
+  address += 1;
+  if (address == EEPROM.length())
+  {
+    address = 0;
+  }
 }
 
 void writeRegister(int CS, char registerAddress, char value)
